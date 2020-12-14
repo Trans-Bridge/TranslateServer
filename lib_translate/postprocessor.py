cb92309ce5532ed6641cf867bd86d405b638e956
hanbing <beatmight@gmail.com>
2020-12-14 09:06:57 +0000

    fix bugs when removing whitespace in zh translation.

--- lib_translate/postprocessor.py
+++ lib_translate/postprocessor.py
@@ -6,18 +6,26 @@ input_type: str
 output_type: str
 """
 import sys
+import re
 
 from config import global_config
 
 # 获取当前模块有用的配置
 postprocess_pipeline = global_config.get("postprocess_pipeline", [])
 
-
 def get_remove_whitespace_postprocessor():
     """
     去除文本中的所有空格，中文按字分开时可以将字符间空格去掉。
+    >>> processor = get_remove_whitespace_postprocessor()
+    >>> text = "本 • 富 兰 克 林 （ Ben Franklin ） 称 德 国 人 愚 蠢 而 剽 悍 。"
+    >>> processor(text)
+    '本•富兰克林（ Ben Franklin ）称德国人愚蠢而剽悍。'
     """
-    def postprocessor(line): return line.replace(" ", "")
+    re_han = re.compile("([^A-Za-z0-9])(\\s+)([^A-Za-z0-9])")
+    def postprocessor(line): 
+        while re_han.search(line):
+            line = re_han.sub("\\g<1>\\g<3>", line)
+        return line
     return postprocessor
 
 
