2153125a50c188b3b76835f8a3d3104264980aff
hanbing <beatmight@gmail.com>
2021-02-19 02:50:51 +0000

    fix: if "device_cuda" in image name, run container with "--runtime=nvidia"

--- docker/test_container.sh
+++ docker/test_container.sh
@@ -3,9 +3,13 @@ set -e
 IMAGE=$1
 MODEL_FOLDER=$2
 CONTAINER_NAME=translate_server_py_ci_$IMAGE
+if [[ $IMAGE =~ "device_cuda" ]]
+then
+  RUNTIME_FLAG="--runtime=nvidia"
+fi
 
 # start the container
-docker run -n $CONTAINER_NAME -p -v ${MODEL_FOLDER}:/root/translate_server_py/mount -itd $IMAGE
+docker run -n $CONTAINER_NAME $RUNTIME_FLAG -p -v ${MODEL_FOLDER}:/root/translate_server_py/mount -itd $IMAGE
 
 # sleep 10 second to wait for service ready
 sleep 10
